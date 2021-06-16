33a96046888d2d7f09d320e022aaa11bf9ffe1d3
Ankit Kumar <ankit628792@gmail.com>
2021-06-16 17:28:01 +0530

    added bookmark feature 2

---
+++ src/Components/Loader.js
@@ -0,0 +1,22 @@
+import React from 'react'
+import './Loader.scss'
+
+function Loader() {
+    return (
+        <div class='loader loader1'>
+            <div>
+                <div>
+                    <div>
+                        <div>
+                            <div>
+                                <div></div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    )
+}
+
+export default Loader
