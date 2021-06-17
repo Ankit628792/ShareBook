6d4462c4dd1659bbef6eadc4ebc62bcbc6d4487b
Ankit Kumar <ankit628792@gmail.com>
2021-06-17 22:38:02 +0530

    remove large screen bug 3

--- src/Views/AllBooks.js
+++ src/Views/AllBooks.js
@@ -16,7 +16,7 @@ function AllBooks() {
         transition={pageTransition}
     >
         <h1 className="text-center py-5 mt-5 text-xl bg-blue-200 font-semibold">Available Books in the Book Shelf</h1>
-        <div className="w-full max-w-full flex flex-wrap py-12 bg-gray-100 xl:px-10">
+        <div className="all_book w-full max-w-full flex flex-wrap py-12 bg-gray-100 xl:px-10">
             {
                 bookList.map((book) => (
                     <Book key={book.ISBN} id={book.ISBN} author={book.author} title={book.title} image={book.image} summary={book.summary} price={book.price} />
