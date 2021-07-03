69285d4ad96624e263b5bc31e0581482f5049122
Ankit Kumar <ankit628792@gmail.com>
2021-07-03 20:27:34 +0530

    server

--- app.js
+++ app.js
@@ -44,49 +44,6 @@ app.get('/getcookie', function (req, res) {
 //    404	Not Found	    When a particular resource doesn’t exist or couldn’t be found.
 
 
-const io = require('socket.io')(port)
-
-console.log('socket connected ')
-let users = []
-const addUser = (userId, socketId) => {
-    !users.some((user) => user.userId === userId) &&
-        users.push({ userId, socketId })
-}
-
-const removeUser = (socketId) => {
-    users = users.filter((user) => user.socketId !== socketId)
-}
-
-const getUser = (userId) => {
-    return users.find(user => user.userId === userId)
-}
-
-// when connect 
-io.on('connection', (socket) => {
-    console.log('a user connected')
-    //take userId and socketId
-	console.log(users);
-    socket.on('addUser', (userId) => {
-        addUser(userId, socket.id)
-        io.emit('getUsers', users)
-    })
-
-    //send and get message
-    socket.on('sendMessage', ({ senderId, receiverId, text }) => {
-        const user = getUser(receiverId)
-
-        io.to(user.socketId).emit('getMessage', {
-            senderId, text,
-        })
-    })
-
-    // when disconnect
-    socket.on('disconnect', () => {
-        removeUser(socket.id)
-        io.emit('getUsers', users)
-    })
-})
-
 if (process.env.NODE_ENV == 'production') {
     const path = require('path');
     app.use(express.static('client/build'))
--- client/src/Components/chat/Chat.js
+++ client/src/Components/chat/Chat.js
@@ -34,7 +34,7 @@ const Chat = () => {
     const [friend, setFriend] = useState([])
 
     useEffect(() => {
-        socket.current = io() ;
+        socket.current = io('https://sharebook-chat.herokuapp.com') ;
         socket.current.on('getMessage', (data) => {
             setArrivalMessage({
                 sender: data.senderId,
