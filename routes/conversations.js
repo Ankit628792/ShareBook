e38ba1bad28e2f114f478393493728ca01b84eed
Ankit Kumar <ankit628792@gmail.com>
2021-07-05 16:03:20 +0530

    mongo

--- routes/conversations.js
+++ routes/conversations.js
@@ -6,15 +6,22 @@ const Conversation = require('../model/userConversation')
 //new conv
 router.post('/', async (req,res) => {
     const {senderId, receiverId} = req.body
-    const newConversation = new Conversation({
-        members: [senderId, receiverId]
-    })
-    try {
-        const savedConversation = await newConversation.save()
-        res.status(200).json(savedConversation)
-    } catch (error) {
-        res.status(500).json(error)
-    }
+    const convExist = await Conversation.findOne({ $or: [ {members: [senderId, receiverId]}, {members: [receiverId, senderId]} ]});
+        if (convExist) {
+            return res.status(200).json({ msg: "conv exist" })
+        }
+        else{
+
+            const newConversation = new Conversation({
+                members: [senderId, receiverId]
+            })
+            try {
+                const savedConversation = await newConversation.save()
+                res.status(200).json(savedConversation)
+            } catch (error) {
+                res.status(500).json(error)
+            }
+        }
 })
 
 //get conv of user
