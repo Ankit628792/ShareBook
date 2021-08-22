ef6b70504765786e8d98ee7bbef60147958212bd
Ankit Kumar <ankit628792@gmail.com>
2021-08-22 11:40:22 +0530

    auto rebuild issue in heroku

--- routes/books.js
+++ routes/books.js
@@ -11,7 +11,7 @@ const Book = require('../model/userBook');
 
 // Store book image using multer
 const storageBook = multer.diskStorage({
-    destination: './client/images/',
+    destination: './books/images/',
     filename: (req, file, cb) => {
         return cb(null, `${Date.now()}${path.extname(file.originalname)}`)
     }
@@ -19,7 +19,7 @@ const storageBook = multer.diskStorage({
 const uploadBook = multer({
     storage: storageBook
 })
-router.use('/client/images', express.static('client/images'))
+router.use('/books/images', express.static('books/images'))
 // Store book image using multer end 
 
 
@@ -68,7 +68,7 @@ router.post('/addbook', uploadBook.single('image_url'), (req, res) => {
     try {
         // const bookId = new Date().getTime().toString();
         const bookId = `${req.file.filename.split('.')[0]}`
-        const image_url = `../images/${req.file.filename}`;
+        const image_url = `../../books/images/${req.file.filename}`;
         console.log(image_url);
         const book = new Book({ userId, username, location, bookId, bookname, image_url, category, condition, description });
         book.save().then((data) => {
