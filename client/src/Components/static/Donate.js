21b5ccedd6248c62871a6117569cf6eed6e2b805
Ankit <ankit628792@gmail.com>
2021-10-27 19:54:44 +0530

    Update Donate.js

--- client/src/Components/static/Donate.js
+++ client/src/Components/static/Donate.js
@@ -4,10 +4,10 @@ const Donate = () => {
     return (
         <>
             <div className="flex justify-center items-center" style={{height: '50vh'}} > 
-                <div className="flex flex-col justify-center items-center p-7 px-10 max-w-lg mx-auto shadow-lg rounded-lg" > 
-                     <h5 className="card-title text-2xl font-medium">Donate Us</h5>
-                     <p className="card-text text-lg my-1">Help us to grow more....</p>
-                     <a href="https://imjo.in/YDpuF6" className="btn btn-primary mt-1">Donate</a>
+                <div className="flex flex-col justify-center items-center p-10 max-w-lg mx-auto shadow-lg rounded-lg" > 
+                     <h5 className="card-title text-2xl sm:text-3xl font-semibold">Donate Us</h5>
+                     <p className="card-text text-lg sm:text-xl my-1 font-medium">Help us to grow more....</p>
+                     <a href="https://imjo.in/YDpuF6" className="btn btn-primary mt-1">Donate Us</a>
                 </div>
             </div>
         </>
