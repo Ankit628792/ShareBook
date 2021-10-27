8411b8ef1700aea411cdca441f2d196bea200870
Ankit <ankit628792@gmail.com>
2021-10-27 18:31:51 +0530

    Update Donate.js

--- client/src/Components/static/Donate.js
+++ client/src/Components/static/Donate.js
@@ -1,17 +1,14 @@
 import React from "react";
 
-const Donate = () =>{
+const Donate = () => {
     return (
         <>
-            
-            <div class="card" style={{display: 'flex',  justifyContent:'center', alignItems:'center', height: '50vh', width:"100vw"}} > 
-                    <div class="card-body">
-                     <div style={{padding:"12px"}}>
-                     <h5 class="card-title">Donate Us</h5>
-                     <p class="card-text">Help us to grow more....</p>
-                     </div>
-                     <a href="https://imjo.in/YDpuF6" class="btn btn-primary">Donate</a>
-                 </div>
+            <div className="flex justify-center items-center" style={{height: '50vh'}} > 
+                <div className="flex flex-col justify-center items-center p-7 px-10 max-w-lg mx-auto shadow-lg rounded-lg" > 
+                     <h5 className="card-title text-2xl font-medium">Donate Us</h5>
+                     <p className="card-text text-lg my-1">Help us to grow more....</p>
+                     <a href="https://imjo.in/YDpuF6" className="btn btn-primary mt-1">Donate</a>
+                </div>
             </div>
         </>
     )
