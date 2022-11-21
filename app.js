d946046ca7f9583d18b9037697f9d9f1c7e9ba03
Ankit Kumar <ankit@sugoilabs.com>
2022-11-21 21:36:25 +0530

    vercel

--- app.js
+++ app.js
@@ -83,8 +83,8 @@ io.on('connection', (socket) => {
 
 if (process.env.NODE_ENV == 'production') {
     const path = require('path');
-    app.use(express.static('client/build'))
+    app.use(express.static('client/client_build'))
     app.get('*', (req, res) => {
-        res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'))
+        res.sendFile(path.resolve(__dirname, 'client', 'client_build', 'index.html'))
     })
 }
