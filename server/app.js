8ce83bc870cd1fa80410b8d10d8f70a7ad3a565c
Ankit Kumar <ankit@sugoilabs.com>
2022-11-21 21:53:17 +0530

    vercel

--- server/app.js
+++ server/app.js
@@ -83,8 +83,8 @@ io.on('connection', (socket) => {
 
 
 if (process.env.NODE_ENV == 'production') {
-    app.use(express.static(path.join(__dirname, '../client', 'build')));
-    app.get('/*', (req, res) => {
-      res.sendFile(path.join(__dirname, '../client', 'build', 'index.html'));
+    app.use(express.static(path.join(__dirname, '../client/build')));
+    app.get('*', (req, res) => {
+      res.sendFile(path.join(__dirname, '../client/build/index.html'));
     })
 }
